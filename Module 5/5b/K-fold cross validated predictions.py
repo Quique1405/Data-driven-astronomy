d9490945fbadeeb6cf92359a38ed6242a203983f
Quique1405 <nemendozar@unal.edu.co>
2025-08-17 19:59:00 -0500

    Cross validation predictions

---
+++ Module 5/5b/K-fold cross validated predictions.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import KFold
+def get_features_targets(data):
+  features = np.zeros((data.shape[0], 4))
+  features[:, 0] = data['u'] - data['g']
+  features[:, 1] = data['g'] - data['r']
+  features[:, 2] = data['r'] - data['i']
+  features[:, 3] = data['i'] - data['z']
+  targets = data['redshift']
+  return features, targets
+def median_diff(predicted, actual):
+  return np.median(np.abs(predicted - actual))
+def cross_validate_predictions(model, features, targets, k):
+    kf = KFold(n_splits=k, shuffle=True)
+
+    # declare an array for predicted redshifts from each iteration
+    all_predictions = np.zeros(shape=len(targets))
+
+    for train_indices, test_indices in kf.split(features):
+        # split the data into training and testing
+        train_features, test_features = features[train_indices], features[test_indices]
+        train_targets, test_targets = targets[train_indices], targets[test_indices]
+    
+        # fit the model for the current set
+        model.fit(train_features, train_targets)
+        # predict using the model
+        predictions = model.predict(test_features)
+        # put the predicted values in the all_predictions array defined above
+        all_predictions[test_indices] = predictions
+
+    # return the predictions
+    return all_predictions
+data = np.load(r'Module 5\5a\sdss_galaxy_colors.npy')
+features, targets = get_features_targets(data)
+dtr=DecisionTreeRegressor(max_depth=19)
+predictions = cross_validate_predictions(dtr, features, targets, 10)
+plt.scatter(targets, predictions, alpha=.5,s=.05,c="black")
+plt.title("K-fold Cross-Validated Redshift Predictions vs Measured Redshift")
+plt.xlim(0)
+plt.xlabel("Measured Redshift")
+plt.ylabel("Predicted Redshift")
+plt.ylim(0)
+plt.show()
